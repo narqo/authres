04508e5c690ad97fd257ed0c8d9330d9c7411854
Christos Vontas <christos@redsift.io>
2017-12-05 16:37:07 +0000

    fixed tests

--- authres_test.go
+++ authres_test.go
@@ -1,16 +1,20 @@
 package authres
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestParseAuthenticationResults(t *testing.T) {
-	parseTests := []struct{
-		in string
+	parseTests := []struct {
+		in      string
 		authres AuthenticationResults
 	}{
 		{
 			`test.example.org 1; none`,
 			AuthenticationResults{
-				AuthServID: "example.com",
+				AuthServID: "test.example.org",
 				Version:    "1",
 			},
 		},
@@ -18,25 +22,60 @@ func TestParseAuthenticationResults(t *testing.T) {
 			`example.com; spf=pass smtp.mailfrom=example.net`,
 			AuthenticationResults{
 				AuthServID: "example.com",
+				Results: []AuthenticationResult{
+					{
+						Method:     "spf",
+						Result:     "pass",
+						Properties: []string{"smtp+++mailfrom+++example.net"},
+					},
+				},
 			},
 		},
 		{
 			`example.com; dkim=pass (good signature) header.d=mail-router.example.net; dkim=fail (bad signature) header.d=newyork.example.com`,
-			AuthenticationResults{},
+			AuthenticationResults{
+				AuthServID: "example.com",
+				Results: []AuthenticationResult{
+					{
+						Method:     "dkim",
+						Result:     "pass",
+						Properties: []string{"header+++d+++mail-router.example.net"},
+					},
+					{
+						Method:     "dkim",
+						Result:     "fail",
+						Properties: []string{"header+++d+++newyork.example.com"},
+					},
+				},
+			},
 		},
 		{
-			`foo.example.net (foobar) 1 (baz); dkim (Because I like it) / 1 (One yay) = (wait for it) fail policy (A dot can go here) . (like that) expired (this surprised me) = (as I wasn't expecting it) 1362471462`,
-			AuthenticationResults{},
+			`foo.example.net (foobar) 1 (baz); dkim (Because I like it) /1 (One yay) = (wait for it) fail policy (A dot can go here) . (like that) expired (this surprised me) = (as I wasn't expecting it) 1362471462`,
+			AuthenticationResults{
+				AuthServID: "foo.example.net",
+				Version:    "1",
+				Results: []AuthenticationResult{
+					{
+						Method:     "dkim",
+						Version:    "1",
+						Result:     "fail",
+						Properties: []string{"policy+++expired+++1362471462"},
+					},
+				},
+			},
 		},
 	}
 
 	for n, tc := range parseTests {
-		p := authresParser{tc.in}
-		res, err := p.ParseAuthenticationResults()
-		if err != nil {
-			t.Errorf("test %d: error: %v", n, err)
-			continue
-		}
-		t.Logf("test %d: authres: %+v", n, res)
+		t.Run(fmt.Sprint(n), func(t *testing.T) {
+			p := authresParser{tc.in}
+			res, err := p.ParseAuthenticationResults()
+			if err != nil {
+				t.Errorf("test %d: error: %v", n, err)
+			}
+			if !reflect.DeepEqual(*res, tc.authres) {
+				t.Errorf("want: %+v, got: %+v", tc.authres, *res)
+			}
+		})
 	}
 }
